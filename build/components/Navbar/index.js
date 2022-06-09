b9ad50d9c97071e2e3bef027dcef7571a7af3117
Marco Bejarano <marco.bejarano03@gmail.com>
2022-06-09 04:13:59 -0700

    created most of the navbar

--- build/components/Navbar/index.js
+++ build/components/Navbar/index.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElement';
+import Link from 'next/link';
+import {FaBars} from 'react-icons/fa';
+
 
 export default function index() {
   return (
     <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo>
-                    dolla
-                </NavLogo>
-            </NavbarContainer>
-        </Nav>
+      <Nav>
+        <NavbarContainer>
+          <Link href="/" passHref>
+            <NavLogo>
+            dolla
+            </NavLogo>
+          </Link>
+          <MobileIcon>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about" smooth={true}>
+                  About
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover" smooth={true}>
+                  Discover
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services" smooth={true}>
+                  Services
+                </NavLinks>
+                </NavItem>
+                <NavItem>
+                <NavLinks to="signup" smooth={true}>
+                  Sign Up
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <Link href="/signin" passHref>
+            <NavBtn>
+                <NavBtnLink>Sign in</NavBtnLink>
+            </NavBtn>
+            </Link>
+        </NavbarContainer>
+      </Nav>
     </>
   )
 }

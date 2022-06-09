576e6785e7094b70d425d621a8c0b2fa056864bc
Marco Bejarano <marco.bejarano03@gmail.com>
2022-06-08 23:25:01 -0700

    created next js app, but am working on figuring out styled componenets

---
+++ build/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>
+                    dolla
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>
+    </>
+  )
+}
---
+++ build/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar/index';
+
+export default function index() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}

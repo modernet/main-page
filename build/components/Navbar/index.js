83f1547074e54bd0e4b3b94c9275518d8cadaaba
Kai Warren <kaiwarren12@yahoo.com>
2022-06-14 20:08:42 -0700

    pushed final changes and edited verbage. Added some on hover css affects

--- build/components/Navbar/index.js
+++ build/components/Navbar/index.js
@@ -1,53 +1,73 @@
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElement';
-import Link from 'next/link';
-import {FaBars} from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElement";
+import Link from "next/link";
+import { FaBars } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
 
+export default function index({ toggle }) {
+  const [scrollNav, setScrollNav] = useState(false);
 
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
 
-export default function index({toggle}) {
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  });
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
           <Link href="/" passHref>
-            <NavLogo>
-            Modernet
-            </NavLogo>
+            <NavLogo>Modernet</NavLogo>
           </Link>
           <MobileIcon onClick={toggle}>
-              <FaBars />
-            </MobileIcon>
-            <NavMenu>
-              <NavItem>
-                <NavLinks to="about" smooth={true}>
-                  About
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="workflow" smooth={true}>
-                  Why Us
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="services" smooth={true}>
-                  Services
-                </NavLinks>
-                </NavItem>
-                <NavItem>
-                <NavLinks to="ecommerce" smooth={true}>
-                  E-commerce
-                </NavLinks>
-              </NavItem>
-            </NavMenu>
-            
-            <NavBtn>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about" smooth={true}>
+                About
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="workflow" smooth={true}>
+                Why Us
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services" smooth={true}>
+                Services
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="ecommerce" smooth={true}>
+                E-Commerce
+              </NavLinks>
+            </NavItem>
+          </NavMenu>
+
+          <NavBtn>
             <Link href="/calendly" passHref>
-                <NavBtnLink>Contact Us</NavBtnLink>
+              <NavBtnLink>Contact Us</NavBtnLink>
             </Link>
-            </NavBtn>
-           
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
-  )
+  );
 }
